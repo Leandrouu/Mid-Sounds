4ef427349d31c0dfcf73feb32f76c6c00af16154
Leandro <leandrossa.2003@gmail.com>
2022-05-17 17:20:17 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+function tocaSom (seletordeAudio) {
+   const elemento = document.querySelector(seletordeAudio);
+   
+
+   if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+   }
+   else{
+    //alert('elemento não encontrado')
+    console.log('Elemento não encontrado ou seletor inválido')
+   }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == 'Space')
+
+
+        if (evento.code == 'espaço' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+   
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
